1056981de264fee61acb12886dcbfd529f379e95
gabridele <gabrieledele@gmail.com>
2025-04-15 16:30:48 +0200

    modified folder structure

--- code_raw/copy_single_scans.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SOURCE_DIR="/home/gabridele/backup/ROSMAP_proc/sourcedata/batch_2/sub-99911705/100106_00_99911705"
-DEST_DIR="."
-
-for file in "$SOURCE_DIR"/*3D*; do
-    rsync -av --copy-links "$file" "$DEST_DIR"
-done
--- code_raw/reset_head.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CWD=$(pwd)
-
-for sub in "$CWD"/sub-*; do
-    cd $sub
-    git reset --hard HEAD
-    cd ..
-done
