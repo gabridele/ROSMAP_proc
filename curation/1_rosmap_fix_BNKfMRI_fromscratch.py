315b0105851d14fbd81e73587e51b8208b6fb9eb
gabridele <gabrieledele@gmail.com>
2025-05-20 15:55:32 +0200

    updated scripts

--- curation/1_rosmap_fix_BNKfMRI_fromscratch.py
+++ curation/1_rosmap_fix_BNKfMRI_fromscratch.py
@@ -142,6 +142,31 @@ def set_xyzt_units(img, xyz='mm', t='sec'):
     header.set_xyzt_units(xyz=xyz, t=t)
     return img.__class__(img.get_fdata().copy(), img.affine, header) 
 
+base_json = {'Modality': 'MR',
+             'MagneticFieldStrength': 1.5,
+             'Manufacturer': 'GE',
+             'ManufacturersModelName': 'SIGNA_EXCITE',
+             'InstitutionName': 'Bannockburn_Radiology_Center',
+             'DeviceSerialNumber': '00000847GURNEEMR',
+             'StationName': 'BRCMR',
+             'PatientPosition': 'HFS',
+             'SoftwareVersions': '11',
+             'TaskName':'rest',
+             'Instructions':'Eyes closed',
+             'EchoTime': 0.0033,
+             'RepetitionTime': 2,
+             'FlipAngle': 85,
+             'PulseSequenceType': '2D Spiral GRE Resting State fMRI',
+             'SliceThickness':5,
+            'SliceEncodingDirection':'k',
+            'SequenceName':'sprlio',
+            'SliceTiming':[0.0,0.08,0.16,0.24,0.32,0.4,0.48,0.56,0.64,0.72,0.8,0.88,0.96,1.04,1.12,1.2,1.28,1.36,1.44,1.52,1.6,1.68,1.76,1.84,1.92,2.0]
+            }
+
+def write_bnk_json(pth,base_json):
+    with open(pth, 'w') as fp:
+        json.dump(base_json, fp,sort_keys=True, indent=4)
+
 def main():
     fmri_files = os.path.join(os.getcwd(), 'sub*', 'ses-*', 'func', '*.zip')
 
@@ -171,12 +196,11 @@ def main():
         reoriented.to_filename(nii)
         
         # dl.save(dataset=sub_id, message=f"Converted rs-fmri scan from analyze format to nifti in {directory_path}")
-
+        jpth = file_path.split('.')[0]+'.json'
+        write_bnk_json(jpth,base_json)
         # get rid of old files
         os.remove(fmri)
 
 if __name__ == "__main__":
     
     main()
-        
-
--- curation/2_modify_intendedfor.py
+++ curation/2_modify_intendedfor.py
@@ -26,26 +26,26 @@ def modify_intended_for(input_file):
     except json.JSONDecodeError:
         print(f"Error decoding JSON in file: {input_file}")
         return
-    
+     
     # Modify the 'INTENDED_FOR' entry with correct func scan
-    if "IntendedFor" in data:
-        if 'BNK' in input_file:
-            data.pop("IntendedFor", None)
-            print(f"Skipping file {input_file} as BNK scans do not need fmap correction")
-            return
-        try:
-            input_dir = os.path.relpath(os.path.dirname(input_file), start=os.getcwd())
-            func_dir = os.path.abspath(os.path.join(input_dir, "../func/"))
-            data["IntendedFor"] = [
-                os.path.relpath(os.path.join(func_dir, file), start=os.path.join(os.getcwd(), input_dir.split("ses-")[0]))
 
-                for file in os.listdir(func_dir) if file.endswith("_bold.nii.gz")
-            ]
-            print(f"Modified 'IntendedFor' entry in {input_file}")
-            print(data["IntendedFor"])
-        except FileNotFoundError:
-            print("Directory '../func' not found.")
-            return
+    if 'BNK' in input_file:
+        data.pop("IntendedFor", None)
+        print(f"Skipping file {input_file} as BNK scans do not need fmap correction")
+        return
+    try:
+        input_dir = os.path.relpath(os.path.dirname(input_file), start=os.getcwd())
+        func_dir = os.path.abspath(os.path.join(input_dir, "../func/"))
+        data["IntendedFor"] = [
+            os.path.relpath(os.path.join(func_dir, file), start=os.path.join(os.getcwd(), input_dir.split("ses-")[0]))
+
+            for file in os.listdir(func_dir) if file.endswith("_bold.nii.gz")
+        ]
+        print(f"Modified 'IntendedFor' entry in {input_file}")
+        print(data["IntendedFor"])
+    except FileNotFoundError:
+        print("Directory '../func' not found.")
+        return
 
     #Save the modified JSON file
     try:
